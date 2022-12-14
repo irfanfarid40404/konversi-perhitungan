ffb8e690ef1b842250c8faf9849e5194e4139f89
irfanfarid40404 <crime14004@gmail.com>
2022-12-14 16:35:16 +0700

    Java Konversi Perhitungan

---
+++ src/mfaridirfanuddin19_lsp/MfaridIrfanuddin19_LSP.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mfaridirfanuddin19_lsp;
+
+/**
+ *
+ * @author ACER
+ */
+public class MfaridIrfanuddin19_LSP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
